c57fcc7a90429a26016dec3cbd3091ac791ee24d
Charles Emrich <libertinistic@gmail.com>
2017-06-02 10:51:12 -0700

    add beginning object code

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 namespace WordCounter.Objects
 {
   public class RepeatCounter
   {
+    private string _corpus;
+    private List<string> _searchTerms = new List<string> {};
+    private Dictionary<string, int> _counts = new Dictionary<string, int> {};
 
+    
   }
 }
